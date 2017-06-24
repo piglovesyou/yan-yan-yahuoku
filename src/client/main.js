b845e34752b87165e9c8d9557683ae71fcc1fb2e
piglovesyou <thepiglovesyou@gmail.com>
2017-06-24 12:53:36 +0900

    style reloader for dev

--- src/client/main.js
+++ src/client/main.js
@@ -11,6 +11,16 @@ Object.assign(Store._state, window.__initialData);
 
 render((
     <BrowserRouter>
-      <Route component={Application} />
+      <Route component={Application}/>
     </BrowserRouter>
 ), document.getElementById('application-container'));
+
+// For dev
+window.startStyleReload = () => {
+  setInterval(() => {
+    Array.from(document.querySelectorAll('link[rel=stylesheet]'))
+        .filter(e => e.href.includes(location.hostname))
+        .map(e => e.href = `${require('url').parse(e.href).pathname}?${Date.now()}`);
+  }, 2000);
+};
+// window.startStyleReload();
