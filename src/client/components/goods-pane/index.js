71cb4daa602507dac70576d418c38facd8324016
piglovesyou <thepiglovesyou@gmail.com>
2017-06-25 21:43:48 +0900

    show details

--- src/client/components/goods-pane/index.js
+++ src/client/components/goods-pane/index.js
@@ -2,14 +2,63 @@ const React = require('react');
 // const {Icon} = require('semantic-ui-react');
 const s = require('./index.scss');
 
-module.exports.default = function GoodsPane(props) {
-  return (
-      <div className={`${s.root}${props.className ? ' ' + props.className : ''}`}>
-        {
-          props.goods ? props.goods.slice(0, 4).map(g => {
-            return <img className={s.img} key={g.AuctionID} src={g.Img.Image1 || g.Image} />;
-          }) : null
-        }
-      </div>
-  );
-};
+const columns = 2;
+const rows = 2;
+
+class GoodsPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hoveredItemId: undefined
+    };
+  }
+
+  render() {
+    const {className, goods} = this.props;
+    return (
+        <div className={`${s.root}${className ? ' ' + className : ''}`}>
+          {
+            goods ? goods.slice(0, 4).map((g, index) => {
+              return (
+                  <div className={s.item}
+                       onMouseEnter={(e) => this.setState({hoveredItemId: g.AuctionID})}
+                       onMouseLeave={(e) => this.setState({hoveredItemId: undefined})}
+                       key={g.AuctionID}>
+                    <img className={s.img} key={g.AuctionID} src={g.Img.Image1 || g.Image}/>
+                    {
+                      this.state.hoveredItemId === g.AuctionID
+                          ? (
+                          <div className={s.itemDetail} style={getLeftAndRight(index)}>
+                            {g.Title}
+                          </div>
+                      )
+                          : null
+                    }
+                  </div>
+              );
+            }) : null
+          }
+        </div>
+    );
+  }
+}
+
+const widthUnit = 100 / columns;
+function getLeftAndRight(index) {
+  const leftIndex = index % columns;
+  const isRightEdge = leftIndex + 1 === columns;
+  if (isRightEdge) {
+    return {
+      right: 100 - widthUnit + '%',
+      left: 100 - (widthUnit * 2) + '%',
+    };
+  } else {
+    const leftNum = leftIndex + 1;
+    return {
+      left: leftNum * widthUnit + '%',
+      right: (columns - leftIndex - 2) * widthUnit + '%',
+    };
+  }
+}
+
+module.exports.default = GoodsPane;
