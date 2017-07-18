7e8ee953ebd67bd4e3d54988b5b05d66af29933b
piglovesyou <thepiglovesyou@gmail.com>
2017-07-19 08:17:14 +0900

    Pass App as a param of Route to give props.history etc.

--- src/client/main.js
+++ src/client/main.js
@@ -11,7 +11,7 @@ Object.assign(Store._state, window.__initialData);
 
 render((
     <BrowserRouter>
-      <Application/>
+      <Route path="/" component={Application}/>
     </BrowserRouter>
 ), document.getElementById('application-container'));
 
