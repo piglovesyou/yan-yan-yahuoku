f56410bce780f804479181f0f86a4c08799c16de
piglovesyou <thepiglovesyou@gmail.com>
2017-07-21 00:34:42 +0900

    img in detail flippable

--- src/client/components/detail/index.js
+++ src/client/components/detail/index.js
@@ -6,10 +6,15 @@ const {goBackFromDetail} = require('../../actions');
 class Detal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      imageIndex: 0
+    };
+    this.flipImage = this.flipImage.bind(this);
   }
 
   render() {
     const i = this.props;
+    const imgSrc = i.Img[Object.keys(i.Img).sort()[this.state.imageIndex]];
     return (
         <div className={s.root}>
           <div className={s.toolbar}>
@@ -25,29 +30,37 @@ class Detal extends React.Component {
               {i.Title}
             </div>
             {/*<div className={s.rightButton}>*/}
-              {/*{i.displayName*/}
-                  {/*? <Popup trigger={<span><Icon name="lightning"/><a href="/auth/logout">{i.displayName}</a></span>}>*/}
-                    {/*<Icon name="bomb"/>Sign out and destroy session*/}
-                  {/*</Popup>*/}
-                  {/*: <Popup content="Sign in to watch items"*/}
-                           {/*trigger={<span><Icon name="privacy"/><a href="/auth/yj">Sign in</a></span>}*/}
-                  {/*/>*/}
-              {/*}*/}
+            {/*{i.displayName*/}
+            {/*? <Popup trigger={<span><Icon name="lightning"/><a href="/auth/logout">{i.displayName}</a></span>}>*/}
+            {/*<Icon name="bomb"/>Sign out and destroy session*/}
+            {/*</Popup>*/}
+            {/*: <Popup content="Sign in to watch items"*/}
+            {/*trigger={<span><Icon name="privacy"/><a href="/auth/yj">Sign in</a></span>}*/}
+            {/*/>*/}
+            {/*}*/}
             {/*</div>*/}
           </div>
 
-          <div className={s.images}>
-            <img className={s.img} src={i.Img.Image1}/>
+          <div className={s.images} onClick={this.flipImage}>
+            <img className={s.img} src={imgSrc}/>
           </div>
 
           <h2 className={s.h2}>
             <a href={i.AuctionItemUrl} target="_blank">{i.Title}</a>
           </h2>
 
-          <div className={s.detail} dangerouslySetInnerHTML={{__html: i.Description}}></div>
+          <div className={s.detail} dangerouslySetInnerHTML={{__html: i.Description}}/>
 
         </div>
     );
+  }
+
+  flipImage() {
+    const {Img} = this.props;
+    const {length} = Object.keys(Img);
+    this.setState({
+      imageIndex: this.state.imageIndex === length - 1 ? 0 : this.state.imageIndex + 1
+    });
   }
 }
 
